4a3495cb01d20aa8b97f18aa89ed02f354c0a647
nakul1504 <nakul.15.correspondence@gmail.com>
2021-01-03 19:06:56 +0530

    First commit on javaLearning.

---
+++ Cookie_SessionManagement/src/com/nakul/AddServlet.java
@@ -0,0 +1,28 @@
+package com.nakul;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+		
+		
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
+		res.sendRedirect("sq");
+	
+	
+	}
+
+}
